7da21f1238f563954b5b6af9920b37390df47a97
Eeshu-Yadav <eeshu2212081@akgec.ac.in>
2025-01-07 16:41:56 +0000

    add the hosts

--- backend/girman_project/settings.py
+++ backend/girman_project/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
@@ -133,3 +133,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
